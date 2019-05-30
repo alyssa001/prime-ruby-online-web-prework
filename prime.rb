bc139e95a95b082ffc534021687eec3bad952229
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 20:33:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,14 @@
-#Alyssa Yesilyurt
-
 def prime?(num)
-  for x in 2..num-1 do
-    if (num % x) == 0
-      return false
-    else
-      return true 
-  end
-  end
-end   
+	if num <= 1
+		return false
+	else
+		for divisor in 2…(num-1)
+			if (num % divisor) == 0
+				return false
+			elsif num <= 0 
+				return false
+			else
+				return true
+			end
+		end
+	end
