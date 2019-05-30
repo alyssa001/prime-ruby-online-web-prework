508703ee98a3bd123f49f4504d5e55d9c48689b7
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 20:21:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(num)
   for divisor in -100000..(num-1)
    if num <= 0
     return false
-  elsif (num % divisor) == 0 && num > 0
+  elsif (num % divisor) == 0 
     return false
   else 
     return true 
