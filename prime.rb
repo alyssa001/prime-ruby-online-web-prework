07df8cf8a0dcb662bf72c8dc0c02aef604cde23d
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 20:28:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,8 @@ def prime?(num)
   (2..number - 1).each do |x|
     if (num % x) == 0
       return false
-
- # elsif (num % divisor) == 0 
-  #  return false
-  #else
-   # return true 
+    else
+      return true 
   end
   end
 end   
