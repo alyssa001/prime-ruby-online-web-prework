0f23a33eff7a1c0ad386fd7ae6fecd5a96c2b475
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 20:14:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@
 def prime?(num)
   for divisor in -100000..(num - 1)
    if (num % divisor) == 0
-    return true
-  elsif num == 0 
     return false
+  elsif num == 0 
+    return true
   else 
-    return false 
+    return true 
    end
   end
 end   
