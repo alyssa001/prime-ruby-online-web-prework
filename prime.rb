0469dc162ebb20b0b307e871b3337e7774ebf8c9
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 20:26:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,15 @@
 
 def prime?(num)
  for divisor in -100000..(num)
- return false if num <= 2
+ return false 
+  if num <= 2
+ else 
+  return true
 
  # elsif (num % divisor) == 0 
   #  return false
   #else
    # return true 
- # end
+  end
   end
 end   
